4bdb6337bfedd468b68e55e8a142e9e2a5f6d748
AleksGus <128379968+AleksGus@users.noreply.github.com>
2023-08-21 19:54:57 +0200

    Update projekt_3.py

--- projekt_3.py
+++ projekt_3.py
@@ -5,108 +5,90 @@ email: aleksgus@seznam.cz
 discord: alexvonhus
 """
 
-from bs4 import BeautifulSoup
 import requests
-import re
-import pandas as pd
-import argparse
+import csv
+from bs4 import BeautifulSoup as BP
+import sys
 
-
-def main_page(url: str, output_file: str) -> None:
+def get_info(district_url, main_code):
     """
-    The main function that performs the whole process of extracting data from the specified URL
-    and saving it to a CSV file.
-    """
-    def load_page(page_url: str) -> BeautifulSoup:
-        """
-        Loads the page using the GET request and converts it to the BeautifulSoup object.
-        """
-        r = requests.get(page_url).text
-        return BeautifulSoup(r, html_parser)
+Funkce pro získání informací o výsledcích hlasování v obci na základě URL a kódu obce.
+"""
+    try:
+        # Získání HTML stránky z URL
+        electweb = requests.get("https://volby.cz/pls/ps2017nss/" + district_url)
+        # Inicializace parseru BeautifulSoup
+        html_parser = BP(electweb.text, "html.parser")
+        el_data = [main_code]  # Inicializace seznamu pro data
+    except requests.exceptions.ConnectionError as e:
+        sys.exit(f"Problem with connection: {e}")
 
-    def get_rows_with_values(web_page_soup: BeautifulSoup) -> list:
-        """
-        Retrieves rows with values from a page using BeautifulSoup.
-        """
-        rows_list = []
-        rows = web_page_soup.find_all("tr")
-
-    # Konstanty
-    html_parser = "lxml"
-    url = "https://volby.cz/pls/ps2017nss/"
-    headers_pattern = re.compile(r".*sa2.*sb3")
-
-    # Definování CSV
-    district_data = []
-    code_column = "Code"
-    name_column = "Location"
-    registered_column = "Registered"
-    envelopes_column = "Envelopes"
-    valid_column = "Valid"
-
-    # [ ZÍSKÁNÍ DAT ]
-    print("Loading data...")
-    district_page = load_page(url)
-    municipalities_table = get_rows_with_values(district_page)
-
-    # Sledování průběhu
-    total_municipalities = len(municipalities_table)
-    completed_municipalities = 0
-
-    # Získání dat z každé obce
-    for municipality in municipalities_table:
-        municipality_data = {}
-
-        # Získání kódu a názvu
-        municipality_data[code_column] = municipality.find("td", class_="cislo").text
-        municipality_data[name_column] = municipality.find("td", class_="overflow_name").text
-
-        # Získání dat o hlasování
-        link = url + municipality.find("a")["href"]
-        municipality_page = load_page(link)
-
-        municipality_data[registered_column] = municipality_page.find("td", headers="sa2").text
-        municipality_data[envelopes_column] = municipality_page.find("td", headers="sa5").text
-        municipality_data[valid_column] = municipality_page.find("td", headers="sa6").text
-
-        # Získat data pro každého kandidáta
-        votes_table = get_rows_with_values(municipality_page)
-        for political_party in votes_table:
-            party_name_column = political_party.find("td", class_="overflow_name").text
-            valid_votes_number = political_party.find("td", headers=headers_pattern).text.replace("\xa0", "")
-            municipality_data[party_name_column] = valid_votes_number
-
-        district_data.append(municipality_data)
-
-        # Vypočítat procent pokroku
-        completed_municipalities += 1
-        percentage_completion = (completed_municipalities / total_municipalities) * 100
-        print(f"{percentage_completion:.2f}%")
-
-    # [ ULOŽENÍ ZÍSKANÝCH DAT DO CSV ]
-    data_frame = pd.DataFrame(district_data)
-    data_frame.to_csv(output_file, index=False, encoding="utf-8")
-    print(f"Data saved to {output_file}")
-
-
-if __name__ == "_main_page_":
-    while True:
-        try:
-            # Přidání argumentů
-            parser = argparse.ArgumentParser(description="Scrap data from given URL and save it to a CSV file.")
-            parser.add_argument('url', type=str, help="URL to scrap data from")
-            parser.add_argument('output_file', type=str, help="Output CSV file name")
-
-            args = parser.parse_args()
-
-            # Zkontroluje, zda zadané URL začíná očekávanou základní URL
-            if not args.url.startswith("https://volby.cz/pls/ps2017nss/"):
-                raise ValueError("URL must start with 'https://volby.cz/pls/ps2017nss/'")
-
-            main_page(args.url, args.output_file)
+    for i in html_parser.find_all("h3"):
+        if "Obec" in i.text:
+            # Získání názvu obce a přidání ho do seznamu
+            el_data.append(i.text.split(":")[-1].strip())
             break
+    # Získání dalších údajů o hlasování a jejich přidání do seznamu
+    el_data.append(html_parser.find("td", attrs={"headers": "sa2"}).text.replace("\xa0", ""))
+    el_data.append(html_parser.find("td", attrs={"headers": "sa3"}).text.replace("\xa0", ""))
+    el_data.append(html_parser.find("td", attrs={"headers": "sa6"}).text.replace("\xa0", ""))
+    for x in html_parser.find_all("td", attrs={"headers": "t1sa2 t1sb3"}):
+        el_data.append(x.text.replace("\xa0", " "))
+    for x in html_parser.find_all("td", attrs={"headers": "t2sa2 t2sb3", "class": "number"}):
+        el_data.append(x.text.replace("\xa0", " "))
+    return el_data
 
-        # Pokud dojde k chybě v bloku try, spustí se tento blok except
-        except Exception as e:
-            print(f"ERROR: {e}")
-            print("Please, try again...")
+# Funkce pro zápis dat do CSV souboru.
+def write_csv(header, el_data, filename):
+    with open(filename + ".csv", "w", newline="\n", encoding="utf-8") as f:
+        entry = csv.writer(f)
+        entry.writerow(header)  # Zápis hlavičky
+        entry.writerows(el_data)  # Zápis dat
+
+# Funkce pro načtení HTML stránky ze zadaného URL.
+def source(url):
+    source = requests.get(url)
+    soup = BP(source.text, "html.parser")
+    return soup
+
+# Funkce pro získání hlavičky s informacemi o hlasování v okrese.
+def header(district_url):
+    electweb = requests.get("https://volby.cz/pls/ps2017nss/" + district_url)
+    html_parser = BP(electweb.text, 'html.parser')
+    district_data = ["code", "Location", "registered voters", "envelopes", "valid"]
+    for x in html_parser.find_all("td", attrs={"headers": "t1sa1 t1sb2"}):
+        district_data.append(x.text)
+    for x in html_parser.find_all("td", attrs={"headers": "t2sa1 t2sb2", "class": ""}):
+        district_data.append(x.text)
+    return district_data
+
+# Funkce pro získání výsledků hlasování v obcích.
+def el_data(source):
+    el_data = [get_info(x.find("a")["href"], x.text) for x in source.find_all("td", attrs={"class": "cislo"})]
+    district_data = header(source.find("td", attrs={"class": "cislo"}).find("a")["href"])
+    return (district_data, el_data)
+
+# Funkce pro kontrolu URL odkazu na výběr okresu.
+def checkurl(url):
+    if "https://volby.cz/pls/ps2017nss/ps32" not in url:
+        print("No reference to district selection page, please try again")
+        return False
+    else:
+        return True
+
+# Hlavní blok kódu pro spuštění skriptu.
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Scrap data from given URL and save it to a CSV file.")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    output_file = sys.argv[2]
+
+    if not checkurl(url):
+        sys.exit(1)
+
+    print("Processing ...")
+    header_data, el_data = el_data(source(url))
+    write_csv(header_data, el_data, output_file)
+    print(f"Done. The output is in file {output_file}.csv.")
